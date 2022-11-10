2fb2d9da519af575f7116b602a6ea95727f037fb
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2022-11-11 00:10:48 +0530

    gym application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Admin from './pages/Admin'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import NoAccess from './pages/NoAccess'
+import SignUp from './pages/SignUp'
+import Error from './pages/Error'
+import './css/App.css'
+import Navbar from './components/Navbar'
+import { GlobalContext } from './Context'
 
-function App() {
+const App = () => {
+  const {admin}=GlobalContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/admin' element={admin?<Admin />:<Navigate to='/noaccess'/>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/noaccess' element={<NoAccess/>}/>
+        <Route path='/*' element={<Error/>}/>
+      </Routes>
+    </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
